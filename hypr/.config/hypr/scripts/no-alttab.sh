288d09a5a84c8f34fa6a05642727acaf8a2d27f6
Nathan <nluciom04@hotmail.com>
2026-09-08 22:19:11 -0300

    chore(hypr): remove instructions from Alt+Tab banner

--- hypr/.config/hypr/scripts/no-alttab.sh
+++ hypr/.config/hypr/scripts/no-alttab.sh
@@ -3,7 +3,7 @@
 # Evita abrir múltiplos banners se a pessoa teclar Alt+Tab repetidamente
 pidof rofi >/dev/null && exit 0
 
-timeout 2 rofi -e $'🚫 AQUI NÃO TEM ALT+TAB 🚫\n\n(Use Super + 1..9)' -theme-str '
+timeout 2 rofi -e "🚫 AQUI NÃO TEM ALT+TAB 🚫" -theme-str '
 window {
     fullscreen: true;
     background-color: rgba(0, 0, 0, 0.85);
@@ -12,7 +12,7 @@ mainbox {
     background-color: transparent;
 }
 textbox {
-    font: "RobotoMono Nerd Font Bold 44";
+    font: "RobotoMono Nerd Font Bold 48";
     text-color: #ff3333;
     horizontal-align: 0.5;
     vertical-align: 0.5;
